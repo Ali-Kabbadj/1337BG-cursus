f759fc0f050a1934e8088e01f907cd5e379cbf02
Ali Kabbadj <akabbadj@e1r2p1.1337.ma>
2022-10-27 22:49:40 +0100

    FIX mbonus part

--- Cursus/Libft/ft_calloc.c
+++ Cursus/Libft/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: akabbadj <akabbadj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/09 21:43:20 by akabbadj          #+#    #+#             */
-/*   Updated: 2022/10/22 08:17:04 by akabbadj         ###   ########.fr       */
+/*   Updated: 2022/10/27 22:47:56 by akabbadj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -15,6 +15,8 @@ void	*ft_calloc(size_t count, size_t size)
 {
 	void	*buffer;
 
+	if (!count || !size || size == SIZE_MAX || count == SIZE_MAX)
+		return (0);
 	buffer = malloc(count * size);
 	if (!buffer)
 		return (0);
--- Cursus/Libft/ft_lstadd_back.c
+++ Cursus/Libft/ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: akabbadj <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/27 14:04:37 by akabbadj          #+#    #+#             */
-/*   Updated: 2022/10/27 15:51:56 by akabbadj         ###   ########.fr       */
+/*   Updated: 2022/10/27 21:47:38 by akabbadj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -15,9 +15,13 @@ void	ft_lstadd_back(t_list  **lst, t_list *new)
 {
 	t_list	*lastnode;
 
-	lastnode = malloc(sizeof(t_list));
-	if (lst && new)
+	if (lst == NULL)
+		(*lst)->next = ft_lstnew(new->content);
+	else if (new == NULL)
+		(*lst)
+	else
 	{
+		lastnode = malloc(sizeof(t_list));
 		lastnode = ft_lstlast(*lst);
 		lastnode->next = new;
 	}
