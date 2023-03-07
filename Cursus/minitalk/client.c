60a84b3882eba33887eddda4f7779b4845b188a4
Ali Kabbadj <akabbadj@e1r2p3.1337.ma>
2023-03-07 18:39:05 +0100

    minitalk norminette

--- Cursus/minitalk/client.c
+++ Cursus/minitalk/client.c
@@ -6,100 +6,96 @@
 /*   By: akabbadj <akabbadj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/01 18:37:39 by akabbadj          #+#    #+#             */
-/*   Updated: 2023/03/07 18:12:55 by akabbadj         ###   ########.fr       */
+/*   Updated: 2023/03/07 18:38:24 by akabbadj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "./printf/ft_printf.h"
 #include "./libft/libft.h"
+#include "./printf/ft_printf.h"
 #include <signal.h>
 #include <stdio.h>
 
-void handle_server_notif(int sig)
+void	handle_server_notif(int sig)
 {
-    static int bytes_cnt;
-    static int bit;
-    
-    if (sig == SIGUSR1)
-    {
-        bit++;
-        ft_printf("Server has recieved %d bit",bit);
-        ft_printf(" of byte No %d.\n",bytes_cnt);
-        if (bit == 8)
-        {
-            bytes_cnt++;
-            bit = 0;
-            ft_printf("\n\n");
-        }
-    }
-    else if (sig == SIGUSR2)
-    {
-        ft_printf("Something When wrong on the server's side");
-        exit(0);
-    }    
+	static int	bytes_cnt;
+	static int	bit;
+
+	if (sig == SIGUSR1)
+	{
+		bit++;
+		ft_printf("Server has recieved %d bit", bit);
+		ft_printf(" of byte No %d.\n", bytes_cnt);
+		if (bit == 8)
+		{
+			bytes_cnt++;
+			bit = 0;
+			ft_printf("\n\n");
+		}
+	}
+	else if (sig == SIGUSR2)
+	{
+		ft_printf("Something When wrong on the server's side");
+		exit(0);
+	}
 }
 
-static int ft_check_pid(char *PID)
+static int	ft_check_pid(char *PID)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (PID[i])
-    {
-        if (ft_isdigit(PID[i]) == 0)
-            return (-1);
-        i++;
-    }
-    return (0);
+	i = 0;
+	while (PID[i])
+	{
+		if (ft_isdigit(PID[i]) == 0)
+			return (-1);
+		i++;
+	}
+	return (0);
 }
 
-void send_bits(int c, int i, int PID)
+void	send_bits(int c, int i, int PID)
 {
-    if (i < 7)
-    {
-        i++;
-        send_bits(c ,i, PID);
-        if ((c & (1 << i)) == 0)
-            kill(PID, SIGUSR1);
-        else
-            kill(PID, SIGUSR2);
-        usleep(22500);
-    }
+	if (i < 7)
+	{
+		i++;
+		send_bits(c, i, PID);
+		if ((c & (1 << i)) == 0)
+			kill(PID, SIGUSR1);
+		else
+			kill(PID, SIGUSR2);
+		usleep(22500);
+	}
 }
 
-
-
-void check_args(int ac, char **av)
+void	check_args(int ac, char **av)
 {
-    if (ac < 3)
-    {
-        ft_printf("Wrong arg number");
-        exit(0);
-    }
-    if (ft_check_pid(av[1]) == - 1)
-    {
-        ft_printf("Wrong PID format");
-        exit(0);
-    }
+	if (ac < 3)
+	{
+		ft_printf("Wrong arg number");
+		exit(0);
+	}
+	if (ft_check_pid(av[1]) == -1)
+	{
+		ft_printf("Wrong PID format");
+		exit(0);
+	}
 }
 
-
-
-int main(int ac, char **av)
+int	main(int ac, char **av)
 {
-    int PID;
-    int i;
-    
-    check_args(ac, av);
-    i = 0;
-    PID = ft_atoi(av[1]); 
-    signal(SIGUSR2, handle_server_notif);
-    signal(SIGUSR1, handle_server_notif);
-    while (av[2][i])
-    {
-        send_bits(av[2][i], -1, PID);
-        i++;
-    }
-    send_bits('\n', -1, PID);
-    return (0);
+	int	pid;
+	int	i;
+
+	check_args(ac, av);
+	i = 0;
+	pid = ft_atoi(av[1]);
+	signal(SIGUSR1, handle_server_notif);
+	signal(SIGUSR2, handle_server_notif);
+	while (av[2][i])
+	{
+		send_bits(av[2][i], -1, pid);
+		i++;
+	}
+	send_bits('\n', -1, pid);
+	return (0);
 }
--- Cursus/minitalk/server.c
+++ Cursus/minitalk/server.c
@@ -6,56 +6,55 @@
 /*   By: akabbadj <akabbadj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/01 18:37:42 by akabbadj          #+#    #+#             */
-/*   Updated: 2023/03/07 18:14:31 by akabbadj         ###   ########.fr       */
+/*   Updated: 2023/03/07 18:36:15 by akabbadj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "./libft/libft.h"
 #include "./printf/ft_printf.h"
 #include <signal.h>
-#include "./libft/libft.h"
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
- void default_handler(int sig , siginfo_t *si, void *data)
+void	default_handler(int sig, siginfo_t *si, void *data)
 {
-    static int bit_number;
-    static char res;
-    
-    (void)data;
-    
-    if (sig == SIGUSR1)
-        res = res << 1;
-    else
-        res =  (res << 1) | 1;
-    bit_number++;
-    if (bit_number == CHAR_BIT)
-    {
-        int a = write(1,&res,1);
-        if (a == -1)
-        {
-            kill(si->si_pid, SIGUSR2);
-            usleep(500);
-            exit(0);
-        }
-        res = 0;
-        bit_number = 0;
-    }
-    kill(si->si_pid, SIGUSR1);
+	static int	bit_number;
+	static char	res;
+	int			a;
+
+	(void)data;
+	if (sig == SIGUSR1)
+		res = res << 1;
+	else
+		res = (res << 1) | 1;
+	bit_number++;
+	if (bit_number == CHAR_BIT)
+	{
+		a = write(1, &res, 1);
+		if (a == -1)
+		{
+			kill(si->si_pid, SIGUSR2);
+			usleep(500);
+			exit(0);
+		}
+		res = 0;
+		bit_number = 0;
+	}
+	kill(si->si_pid, SIGUSR1);
 }
 
-int main(void)
+int	main(void)
 {
-    int PID;
-    struct sigaction sa;
-    
-    memset(&sa, 0, sizeof(sa));
-    sa.sa_flags = SA_SIGINFO;
-    sa.sa_sigaction = default_handler;
-    PID = getpid();
-    ft_printf("%d\n",PID);
-    sigaction(SIGUSR1, &sa , 0);
-    sigaction(SIGUSR2, &sa , 0);
-    while (1)
-        pause();
+	int					pid;
+	struct sigaction	sa;
+
+	memset(&sa, 0, sizeof(sa));
+	sa.sa_flags = SA_SIGINFO;
+	sa.sa_sigaction = default_handler;
+	pid = getpid();
+	ft_printf("%d\n", PID);
+	sigaction(SIGUSR1, &sa, 0);
+	sigaction(SIGUSR2, &sa, 0);
+	while (1)
+		pause();
 }
